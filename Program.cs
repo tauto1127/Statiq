fab3bdafe8fc0571fecdfb1177fbe270b52349b3
takuto1127 <tiharu717@gmail.com>
2023-10-23 03:55:22 +0000

    firstCommit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Statiq.App;
+using Statiq.Web;
+using System.Threading.Tasks;
+
+namespace SiteGenFromMarkdown
+{
+	class Program
+	{
+		static async Task<int> Main(string[] args)
+		{
+			return await Bootstrapper
+				.Factory
+				.CreateWeb(args)
+				.RunAsync();
+		}
+	}
+}
